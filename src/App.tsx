1ac9b080b18abbb14eae624a217bb912765d90fa
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-03-12 00:19:54 +0500

    responsive mobile

--- src/App.tsx
+++ src/App.tsx
@@ -42,11 +42,11 @@ const App = (): JSX.Element => {
       <div className='w-100  pt-4 d-flex justify-content-center align-items-center'>
         <div className='w-50 d-flex justify-content-center rounded flex-column p-3 align-items-center wrap'>
           <h1 className='text-center fw-bold py-2 w-50' style={{ color: "#ff0000" }}>App todo</h1>
-          <div className='w-50'>
+          <div className='w-100'>
             <label htmlFor="user" className='w-100 my-3'>
               <input type="text" style={{ color: "#ff0000" }} id='user' className='form-control' value={title} onChange={changeHandler} placeholder='Text example' />
             </label>
-            <button className='btn w-25' style={{ color: "#ff0000", background: "#2c2f36" }} onClick={submit}>Add Task</button>
+            <button className='btn w-100' style={{ color: "#ff0000", background: "#2c2f36" }} onClick={submit}>Add Task</button>
             <div className='div d-flex flex-column mt-4 border shadow p-2 rounded align-items-start justify-content-between w-100'>
               {arr.map((c) => (
                 <div key={c.id} style={{ background: "#fafafa" }} className='d-flex rounded w-100 border shadow my-2 py-2 px-2 justify-content-between align-items-center'>
